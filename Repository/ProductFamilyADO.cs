3acde2c058b13972b3620ef94d7fefb215791460
ElSuperMagikarp <uriens2006@gmail.com>
2025-10-22 17:25:21 +0200

    GetAll afegit a ProductFamilyADO

--- Repository/ProductFamilyADO.cs
+++ Repository/ProductFamilyADO.cs
@@ -21,6 +21,29 @@ static class ProductFamilyADO
         Console.WriteLine($"{rows} fila inserida.");
         dbConn.Close();
     }
+
+    public static List<ProductFamily> GetAll(DatabaseConnection dbConn)
+    {
+        List<ProductFamily> productfamilies = new();
+
+        dbConn.Open();
+        string sql = "SELECT Id, Name FROM ProductFamilies";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        using SqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            productfamilies.Add(new ProductFamily
+            {
+                Id = reader.GetGuid(0),
+                Name = reader.GetString(1)
+            });
+        }
+
+        dbConn.Close();
+        return productfamilies;
+    }
 }
 
 /*CREATE TABLE ProductFamilies (
