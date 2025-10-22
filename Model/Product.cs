e0ecf6afd486e74a310b36991bcb48a1bac70b93
ElSuperMagikarp <uriens2006@gmail.com>
2025-10-22 18:24:25 +0200

    Discount afegit al model de Product

--- Model/Product.cs
+++ Model/Product.cs
@@ -7,4 +7,5 @@ public class Product
     public string Code { get; set; } = "";
     public string Name { get; set; } = "";
     public decimal Price { get; set; }
+    public decimal Discount { get; set; }
 }
