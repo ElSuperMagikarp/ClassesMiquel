e5a0f3848cd87ddaa9d52f9d425791f810376c8b
ElSuperMagikarp <uriens2006@gmail.com>
2025-10-22 19:49:08 +0200

    ShoppingCartADO afegit amb Insert

---
+++ Repository/ShoppingCartADO.cs
@@ -0,0 +1,28 @@
+using Microsoft.Data.SqlClient;
+
+using StoreProject.Model;
+using StoreProject.Services;
+
+namespace StoreProject.Repository;
+
+static class ShoppingCartADO
+{
+    public static void Insert(DatabaseConnection dbConn, ShoppingCart shoppingCart)
+    {
+        dbConn.Open();
+
+        string sql = @"INSERT INTO ShoppingCarts (Id)
+                        VALUES (@Id)";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", shoppingCart.Id);
+
+        int rows = cmd.ExecuteNonQuery();
+        Console.WriteLine($"{rows} fila inserida.");
+        dbConn.Close();
+    }
+}
+
+/*CREATE TABLE ShoppingCarts (
+    Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY
+);*/
