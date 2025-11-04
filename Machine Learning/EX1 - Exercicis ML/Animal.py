e3ed8bd364c1b2af58b0f2ba750955ee8a96e21e
ElSuperMagikarp <uriens2006@gmail.com>
2025-11-04 17:08:01 +0100

    Algoritme Animal afegit a MachineLearning

---
+++ Machine Learning/EX1 - Exercicis ML/Animal.py
@@ -0,0 +1,23 @@
+import argparse
+
+def classifica_animal(pel, pes):
+    if pel == 1 and pes < 8:
+        return "Gat"
+    elif pel == 1 and pes >= 8:
+        return "Gos"
+    elif pes < 1:
+        return "Ocell"
+    else:
+        return "Gallina"
+
+parser = argparse.ArgumentParser("")
+
+parser.add_argument("pel", help="Si l'animal te pel o no", type=int)
+parser.add_argument("pes", help="Pes del animal amb kgs", type=int)
+
+args = parser.parse_args()
+
+pel = args.pel
+pes = args.pes
+
+print("Pel: ",classifica_animal(pel,pes))
