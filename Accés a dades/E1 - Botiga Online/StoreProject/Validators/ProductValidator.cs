9404bc52a3f3d261e353e3e313c365619a38cc23
ElSuperMagikarp <uriens2006@gmail.com>
2026-01-13 15:56:20 +0100

    ProductValidator

---
+++ Accés a dades/E1 - Botiga Online/StoreProject/Validators/ProductValidator.cs
@@ -0,0 +1,38 @@
+namespace StoreProject.Validators;
+
+using StoreProject.Common;
+using StoreProject.DTO;
+
+public static class ProductValidator
+{
+    public static Result Validate(ProductRequest product)
+    {
+        if (string.IsNullOrEmpty(product.Code))
+        {
+            return Result.Failure("El codi del producte és obligatori", "DADA_OBLIGATORIA");
+        }
+
+        if (string.IsNullOrEmpty(product.Name))
+        {
+            return Result.Failure("El nom del producte és obligatori", "DADA_OBLIGATORIA");
+        }
+
+        if (product.Price <= 0)
+        {
+            return Result.Failure("El preu ha de ser superior a 0", "PREU_INCORRECTE");
+        }
+
+        if (product.Discount < 0)
+        {
+            return Result.Failure("El descompte ha de ser com a mínim 0", "DESCOMPTE_INCORRECTE");
+        }
+
+        if (product.Discount > 100)
+        {
+            return Result.Failure("El descompte no pot ser superior a 100", "DESCOMPTE_INCORRECTE");
+        }
+
+        return Result.Ok();
+    }
+
+}
