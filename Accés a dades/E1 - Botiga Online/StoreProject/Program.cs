b10b15ed5915a09bf83ab695df54ceb308518957
ElSuperMagikarp <uriens2006@gmail.com>
2026-02-10 16:52:19 +0100

    Configuració JSON Web Token afegit a Program.cs

--- Accés a dades/E1 - Botiga Online/StoreProject/Program.cs
+++ Accés a dades/E1 - Botiga Online/StoreProject/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using StoreProject.Infraestructure.Services;
 using StoreProject.Application.EndPoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,33 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .SetBasePath(AppContext.BaseDirectory)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+// JSON Web Token
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration["Jwt:JwtSecretKey"]!)
+                ),
+
+            ValidateIssuer = true,
+            ValidIssuer = "demo",
+
+            ValidateAudience = true,
+            ValidAudience = "public",
+
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.FromSeconds(30)
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 DatabaseConnection dbConn = new DatabaseConnection(connectionString);
